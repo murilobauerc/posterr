00f36dcdae2396e37077fce35e523832acb88108
murilobauerc <murilobauerc@gmail.com>
2024-07-15 18:16:12 -0300

    Adds default project structure for posterr-front

---
+++ posterr-front/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Posts from "./components/Posts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Posterr</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
